f022f2c4707afdc28056feaa79792ccb80c7620b
DarkOnion0 <Darkgenius1@protonmail.com>
2021-02-26 16:34:21 +0100

    update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,32 @@
-# ~/.config/fish/config.fish
+set fish_greeting                                 # Supresses fish's intro message
+set EDITOR "nvim ''"                 		      # $EDITOR use Emacs in terminal
+set VISUAL "codium "              		          # $VISUAL use Emacs in GUI mode
 
-# STARTSHIP
+
+#
+# RANDOM COLORSCHEME ==> https://gitlab.com/dwt1/shell-color-scripts
+#
+colorscript random 
+
+#
+# CUSTOM MANPAGER
+#
+set -x MANPAGER "sh -c 'col -bx | batcat -l man -p'" 
+
+#
+# STARSHIP PROMPT
+#
+
 starship init fish | source
 
+#
 # ALIAS
-alias l='exa -la'
-alias sudo='doas -- '
-alias e='vscodium '
+#
+
+
+# alias e='vscodium ' # VSCodium name in ArchLinux
+
+alias e='codium '       # VSCodium name in Ubuntu
+alias l='exa -la '      # shortcut with exa 
+alias c='batcat '       # shortcut for bat 
+alias cat='c '          # replace cat by bat
