8d7e5f48a857580875068d7e8c78f7aaf947381f
DarkOnion0 <Darkgenius1@protonmail.com>
2021-02-27 13:44:27 +0100

    change how the script work

--- installer/setup.py
+++ installer/setup.py
@@ -4,44 +4,45 @@
 # TODO test the project
 #########################################
 
-import os
-with open("/etc/os-release", "r") as f:
-    os_info = f.read()
-    os_info = os_info.split("\n")
-        
-    count = 0
-    while count < len(os_info) - 1:
-        
-        tmp = os_info[count].split("=")
-        os_name_tmp = list(tmp[1])
-        
-        if count == 0:
-            #print(os_name_tmp)
-            count2 = 1
-            tmp_arg2 = str()
+#import os
+#with open("/etc/os-release", "r") as f:
+#    os_info = f.read()
+#    os_info = os_info.split("\n")
+#        
+#    count = 0
+#    while count < len(os_info) - 1:
+#        
+#        tmp = os_info[count].split("=")
+#        os_name_tmp = list(tmp[1])
+#        
+#        if count == 0:
+#            #print(os_name_tmp)
+#            count2 = 1
+#            tmp_arg2 = str()
+#
+#            while count2 < len(os_name_tmp) - 1:
+#                tmp_arg2 += os_name_tmp[count2]
+#                
+#                count2 += 1
+#        
+#            #print(tmp_arg2)
+# 
+#        os_info[count] = [tmp[0], tmp_arg2]
+#        
+#        count += 1
+#
+##print(os_info, type(os_info))
+#os_name = os_info[0][1]
+##print(os_name, type(os_name), os_name_tmp, type(os_name_tmp))
+#
+#if os_name == "Pop!_OS" or os_name == "Ubuntu":
+#
+#    os.system("sudo apt -qq install -y python3-pip")
+#    os.system("pip3 install pyyaml rich")
 
-            while count2 < len(os_name_tmp) - 1:
-                tmp_arg2 += os_name_tmp[count2]
-                
-                count2 += 1
-        
-            #print(tmp_arg2)
- 
-        os_info[count] = [tmp[0], tmp_arg2]
-        
-        count += 1
+import yaml, os, rich
 
-#print(os_info, type(os_info))
-os_name = os_info[0][1]
-#print(os_name, type(os_name), os_name_tmp, type(os_name_tmp))
-
-if os_name == "Pop!_OS" or os_name == "Ubuntu":
-
-    os.system("sudo apt -qq install -y python3-pip")
-    os.system("pip3 install pyyaml rich")
-    import yaml
-    import rich
-
+def ubuntu():
     with open("apps.yml", "r") as f:
         app_list = dict(yaml.safe_load(f))
 
@@ -114,5 +115,4 @@ if os_name == "Pop!_OS" or os_name == "Ubuntu":
 
 
 
-else:
-    print(f"Your OS {os_name} with python type = {type(os_name)} is not supported")
+    
