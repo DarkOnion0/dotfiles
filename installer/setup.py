dd27ea66a5120f9062c919277aac48e188203446
DarkOnion0 <Darkgenius1@protonmail.com>
2021-02-27 09:29:58 +0100

    update setup.py

--- installer/setup.py
+++ installer/setup.py
@@ -27,13 +27,8 @@ with open("/etc/os-release", "r") as f:
 
 #print(os_info, type(os_info))
 os_name = os_info[0][1]
-#os_name = "Pop!_OS"
 #print(os_name, type(os_name), os_name_tmp, type(os_name_tmp))
 
-#if str(os_name) == "Pop!_OS":
-#    print("Hello World")
-
-#if == "ubuntu":
 if os_name == "Pop!_OS" or os_name == "Ubuntu":
 
     os.system("sudo apt -qq install -y python3-pip")
@@ -44,36 +39,74 @@ if os_name == "Pop!_OS" or os_name == "Ubuntu":
     with open("apps.yml", "r") as f:
         app_list = dict(yaml.safe_load(f))
 
-    print(app_list)
+    #print(app_list)
 
-    try:
+    try: # try to add listed repository
         repos_tmp = app_list.get("ubuntu")
         repos = repos_tmp.get("repos")
-        #repos_tmp = list(repos_tmp.items())
-        #repos_keys = list(repos.keys())
 
-        print(repos, len(repos))
-        #print(repos_keys)
+        #print(repos, len(repos))
 
-        for key in repos:
-            print("KEY ===> ", key)
+        for key in repos: # get the repos name
+            #print("KEY ===> ", key)
             command_list = repos.get(key)
-            print("\nCOMMAND_LIST ===> ", command_list)
-            #for command in command_list:
-            #command_list = command_list[0]
-            print("\n", command_list)
-        
-            for command in command_list:
-                print("\n\n", command)
+            #print("\nCOMMAND_LIST ===> ", command_list)
+            #print("\n", command_list)
+
+            for command in command_list: # get all the command listed in the repo
+                #print("\n\n", command)
                 os.system(f"{command}")
 
                 #print(key, command)
+            
+            os.system("sudo apt -qq update")
 
-            print(command_list)
-            #os.system(f"{command[0]}")
-    
+            #print(command_list)
+
     except:
         rich.print("[bold red]ERROR:[/bold red] You haven't setup any additional repository")
+        pass
+
+    try:
+        software = repos_tmp.get("packages")
+        
+        try:
+            software_deb = software.get("deb")
+            for apps in software_deb:
+                #print(apps)
+                os.system(f"sudo apt -qq install -y {apps}")
+        except:
+            pass
+        
+        try:
+            software_flatpak = software.get("flatpak")
+            for apps in software_flatpak:
+                #print(apps)
+                os.system(f"flatpak install {apps}")
+        except:
+            pass
+        
+        try:
+            software_src = software.get("external")
+            for key in software_src:
+                software_src_app = software_src.get(key)
+
+                for command in software_src_app:
+                    os.system(f"cd /tmp/ && {command}")
+
+            #for apps in software_src:
+            #    #print(apps)
+            #    os.system(f"{apps}")
+        except:
+            pass
+
+        
+        #print(software)
+    except:
+        rich.print("[bold red]ERROR:[/bold red] An error occurred when installing packages")
+        pass
+
+
 
 else:
     print(f"Your OS {os_name} with python type = {type(os_name)} is not supported")
