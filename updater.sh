2820b78ec83c71cfc603a9b5db5d50ad3e652bb0
DarkOnion0 <Darkgenius1@protonmail.com>
2021-03-07 14:57:57 +0100

    add updater

---
+++ updater.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ~/.config/i3/config ~/Programmation/git/dotfiles/.config/i3/
+cp ~/.config/fish/* ~/Programmation/git/dotfiles/.config/fish/
+cp ~/.config/rofi/* ~/Programmation/git/dotfiles/.config/rofi/
