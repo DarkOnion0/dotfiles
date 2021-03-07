6e890aeadd1c9f41eba798277885f0caf017462c
DarkOnion0 <Darkgenius1@protonmail.com>
2021-03-07 19:57:36 +0100

    update the script

--- updater.sh
+++ updater.sh
@@ -3,3 +3,4 @@
 cp ~/.config/i3/config ~/Programmation/git/dotfiles/.config/i3/
 cp ~/.config/fish/* ~/Programmation/git/dotfiles/.config/fish/
 cp ~/.config/rofi/* ~/Programmation/git/dotfiles/.config/rofi/
+cp ~/.config/polybar/* ~/Programmation/git/dotfiles/.config/polybar/
