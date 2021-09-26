0cf06956455f598e2d806b9c412ec907cd126717
DarkOnion0 <Darkgenius1@protonmail.com>
2021-09-26 18:25:30 +0200

    update installer

--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.16
 
 require (
 	github.com/AlecAivazis/survey/v2 v2.3.2
-	github.com/fatih/color v1.12.0
+	github.com/fatih/color v1.13.0
+	github.com/goccy/go-yaml v1.9.3 // indirect
+	github.com/mattn/go-colorable v0.1.10 // indirect
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
+	golang.org/x/sys v0.0.0-20210925032602-92d5a993a665 // indirect
 )
--- installer/cmd/install.go
+++ installer/cmd/install.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+Copyright © 2021 DarkOnion0
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,13 +17,18 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 
 	"github.com/fatih/color"
 
-	"os/exec"
+	"github.com/goccy/go-yaml"
 )
 
 // installCmd represents the install command
@@ -52,29 +57,41 @@ func init() {
 }
 
 // TYPE
-type answerVersionT struct {
-		SelectedVersion string `survey:"version"`
-	}
 
-	type answersInstallT struct {
-		Hostname string
-		Os       string
-		Version  *answerVersionT
-	}
+type answersInstallT struct {
+	Hostname string
+	Os       string
+}
 
-	type osListT struct {
-		ubuntu []string
-	}
+type answersUbuntuT struct {
+	InstallPackage      bool
+	ImportConfiguration bool
+	SelectedVersion     string `survey:"version"`
+}
 
+type answersConfigurationT struct {
+	SelectedConfiguration []string
+}
+type osListT struct {
+	ubuntu []string
+}
+
+type packageYamlT struct {
+	Ubuntu struct {
+		Prerequisites []string
+		Repos         []string
+		Package       []string
+	}
+}
+
+// type callBackT string
 
 func launchInstaller() {
-	
+	statusDisplay := color.New(color.FgWhite).Add(color.Bold)
 
 	osList := osListT{ubuntu: []string{"21.04"}}
 
-	answersVersion := answerVersionT{}
-
-	answersInstall := answersInstallT{Version: &answersVersion}
+	answersInstall := answersInstallT{}
 
 	var qsInstall = []*survey.Question{
 		{
@@ -96,17 +113,19 @@ func launchInstaller() {
 	err := survey.Ask(qsInstall, &answersInstall)
 
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println("error during setup :", err.Error())
 		return
 	}
 
 	if answersInstall.Os == "archlinux" {
-		answersInstall.Version.SelectedVersion = "rolling"
-		fmt.Println(answersInstall, *answersInstall.Version)
+		// Archlinux specific configuration
+		//fmt.Println(answersInstall, *answersInstall.Version)
+		statusDisplay.Println("😕 ARCHLINUX is currently not configured, sorry")
 
 	} else if answersInstall.Os == "ubuntu" {
-
-		var qsVersion = []*survey.Question{
+		// Ubuntu specific configuration
+		answerUbuntu := answersUbuntuT{}
+		var qsUbuntu = []*survey.Question{
 			{
 				Name: "version",
 				Prompt: &survey.Select{
@@ -115,30 +134,120 @@ func launchInstaller() {
 					Default: osList.ubuntu[0],
 				},
 			},
+			{
+				Name: "installPackage",
+				Prompt: &survey.Confirm{
+					Message: "Do you want to install package ?",
+				},
+			},
+			{
+				Name: "importConfiguration",
+				Prompt: &survey.Confirm{
+					Message: "Do you want to install configuration ?",
+				},
+			},
 		}
 
-		err := survey.Ask(qsVersion, &answersVersion)
+		err := survey.Ask(qsUbuntu, &answerUbuntu)
 
 		if err != nil {
 			fmt.Println(err.Error())
 			return
 		}
-
-		installPackage(answersInstall)
-
+		downloadRepo(answersInstall.Os)
+		installPackage(answersInstall.Os, answerUbuntu.SelectedVersion, answerUbuntu.InstallPackage)
+		importConfiguration(answerUbuntu.ImportConfiguration)
+		if answersInstall.Hostname != "" {
+			editHostname(answersInstall.Hostname)
+		}
 		// fmt.Println(osList.ubuntu, answersInstall, *answersInstall.Version)
 	} else {
 		boldRed := color.New(color.FgRed).Add(color.Bold)
 		boldRed.Println("ERROR: invalid os")
 	}
+
 }
 
-func installPackage(installInfo answersInstallT) {
+func installPackage(os, version string, wantToInstallPackage bool) {
+	if wantToInstallPackage {
+		data, err := ioutil.ReadFile("/tmp/dotfiles/installer/package.yaml")
+
+		if err != nil {
+			fmt.Printf("\n%s", err)
+			return
+		} else {
+
+			packageList := packageYamlT{}
+
+			if err := yaml.Unmarshal([]byte(data), &packageList); err != nil {
+				fmt.Printf("%s", packageList)
+			}
+		}
+	}
+}
+
+func importConfiguration(wantToImportConfiguration bool) {
+	fmt.Println("importConfiguration")
+	answersConfiguration := answersConfigurationT{}
+
+	var qsConfiguration = []*survey.Question{
+		{
+			Name: "SelectedConfiguration",
+			Prompt: &survey.Select{
+				Message: "Choose your configuration file to import:",
+				// Options: osList.ubuntu,
+				// Default: osList.ubuntu[0],
+			},
+		},
+	}
+
+	err := survey.Ask(qsConfiguration, &answersConfiguration)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+}
+
+func editHostname(hostname string) {
 	statusDisplay := color.New(color.FgWhite).Add(color.Bold)
 
-	if len(installInfo.Hostname) != 0 {
+	if len(hostname) != 0 {
 		statusDisplay.Println("\n🏟️  editing hostname...")
-		out, err := exec.Command("echo", installInfo.Hostname).Output()
+
+		backupFile("/etc/hostname", "/etc/hostname.old")
+
+		file, err := os.Create("/etc/hostname")
+
+		if err != nil {
+			log.Fatalf("failed creating file: %s", err)
+		}
+
+		defer file.Close()
+
+		len, err := file.WriteString(strings.ToLower(hostname))
+
+		if err != nil {
+			fmt.Printf("\nLength: %d bytes", len)
+			fmt.Printf("\nFile Name: %s", file.Name())
+			log.Fatalf("failed writing to file: %s", err)
+		} else {
+			statusDisplay.Println("🏟️  editing hostname finished successfully !!!")
+		}
+	}
+}
+
+func backupFile(oldPath, newPath string) {
+	e := os.Rename(oldPath, newPath)
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func downloadRepo(os string) {
+	if os == "ubuntu" {
+		fmt.Println("\n💾 installing git...")
+		out, err := exec.Command("apt", "install", "git").Output()
 
 		if err != nil {
 
@@ -147,7 +256,52 @@ func installPackage(installInfo answersInstallT) {
 			fmt.Println(output)
 
 		} else {
-			statusDisplay.Println("🏟️  editing hostname finished successfully !!!")
+			fmt.Println("💾 installing git finished successfully !!!")
+		}
+	} else {
+		fmt.Printf("Your os %q is not supported", os)
+		return
+	}
+
+	files, err := ioutil.ReadDir("/tmp/")
+	alreadyDownloaded := false
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if f.Name() == "dotfiles" {
+			alreadyDownloaded = true
+		}
+	}
+
+	if alreadyDownloaded {
+		fmt.Println("📂 updating GitHub repo...")
+		out, err := exec.Command("git", "pull", "/tmp/dotfiles").Output()
+
+		if err != nil {
+
+			fmt.Printf("\n%s", err)
+			output := string(out[:])
+			fmt.Println(output)
+
+		} else {
+			fmt.Println("📂 updating GitHub repo finished successfully !!!")
+		}
+	} else {
+
+		fmt.Println("📂 downloading GitHub repo...")
+		out, err := exec.Command("git", "clone", "https://github.com/DarkOnion0/dotfiles.git", "/tmp/dotfiles").Output()
+
+		if err != nil {
+
+			fmt.Printf("\n%s", err)
+			output := string(out[:])
+			fmt.Println(output)
+
+		} else {
+			fmt.Println("📂 downloading GitHub repo finished successfully !!!")
 		}
 	}
 }
