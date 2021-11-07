244af7ee9568a293cc75c44e27fed7849bbae981
DarkOnion0 <Darkgenius1@protonmail.com>
2021-11-07 20:00:18 +0100

    update my fish config for nixos

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,6 +1,6 @@
-set fish_greeting                         # Supresses fish's intro message
-set EDITOR "nvim ''"                 	    # $EDITOR use Nvim in terminal
-set VISUAL "codium "              		    # $VISUAL use VSCodium in GUI mode
+set fish_greeting                         		# Supresses fish's intro message
+set EDITOR "nvim ''"                 	    		# $EDITOR use Nvim in terminal
+set VISUAL "codium "              		    	# $VISUAL use VSCodium in GUI mode
 
 
 #welcome.py
@@ -8,19 +8,19 @@ set VISUAL "codium "              		    # $VISUAL use VSCodium in GUI mode
 #
 # RANDOM COLORSCHEME ==> https://gitlab.com/dwt1/shell-color-scripts
 #
-colorscript random 
+#colorscript random 
 
 # set the default nodejs version to the latest one available
-echo
-nvm install latest
+#echo
+#nvm install latest
 
 #
 # CUSTOM MANPAGER
 #
-set -x MANPAGER "sh -c 'col -bx | batcat -l man -p'"
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 
-set -x SPICETIFY_INSTALL "/home/dark/spicetify-cli"
-set -x PATH "$SPICETIFY_INSTALL:$PATH"
+#set -x SPICETIFY_INSTALL "/home/dark/spicetify-cli"
+#set -x PATH "$SPICETIFY_INSTALL:$PATH"
 
 #
 # STARSHIP PROMPT
@@ -36,9 +36,9 @@ starship init fish | source
 # alias e='vscodium ' 	                      # VSCodium name in ArchLinux
 alias e='codium ' 	                          # VSCodium name in Ubuntu
 alias l='exa -la '	                          # set an alias for the l key
-alias c='batcat '
+alias c='bat '
 
-#alias cat='batcat '
+#alias cat='bat '
 
 alias vim='nvim '	                            # Replace vim by neovim
 alias vi='nvim '	                            # Replace vi by neovim
@@ -51,3 +51,8 @@ alias kscan="kubescape scan framework nsa "   # Alias for the scanning k8s clust
 alias tn="tmux new "                          # Create a new tmux session easly
 alias ta="tmux attach "                       # Attach a tmux session easly
 alias tk="tmux kill-session -t "              # Shortenf to dete tmux session
+
+alias nixos-edit="sudo nvim /etc/nixos/configuration.nix"
+alias nixos-switch="sudo nixos-rebuild switch"
+alias nixos-clear="sudo nix-collect-garbage"
+alias nixos-update="sudo nixos-rebuild switch --upgrade"
