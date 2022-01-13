da410e7023c651c6709419722430503958d32808
DarkOnion0 <Darkgenius1@protonmail.com>
2022-01-13 20:23:18 +0100

    update nixos config with a new kernel

--- home/nixos/system/boot.nix
+++ home/nixos/system/boot.nix
@@ -1,7 +1,9 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_zen;
 }
