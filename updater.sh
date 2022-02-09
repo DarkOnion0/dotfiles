354a8da725ca3cd310cc68b1e5465d66bece8f34
DarkOnion0 <Darkgenius1@protonmail.com>
2022-02-09 16:39:08 +0100

    update the updater script

--- updater.sh
+++ updater.sh
@@ -2,13 +2,14 @@
 
 git status
 
-#cp ~/.config/i3/config ~/Programmation/git/dotfiles/.config/i3/
+cp ~/.config/i3/config ~/Programmation/git/dotfiles/home/.config/i3/
 cp ~/.config/fish/config.fish ~/Programmation/git/dotfiles/home/.config/fish/
 #cp ~/.config/rofi/* ~/Programmation/git/dotfiles/.config/rofi/
 #cp ~/.config/polybar/* ~/Programmation/git/dotfiles/.config/polybar/
 cp ~/.config/starship.toml ~/Programmation/git/dotfiles/home/.config/
 cp ~/.config/alacritty/alacritty.yml ~/Programmation/git/dotfiles/home/.config/alacritty/
 cp ~/.tmux.conf ~/Programmation/git/dotfiles/home/
-sudo cp /etc/nixos/configuration.nix ~/Programmation/git/dotfiles/home/nixos/
+cp ~/.gitconfig ~/Programmation/git/dotfiles/home/
+sudo cp -r /etc/nixos/* ~/Programmation/git/dotfiles/home/nixos/
 
 git status
