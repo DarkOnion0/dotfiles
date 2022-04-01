0ecfd5e5ac5d2eca1e5644a3d7c5e6eee286334c
DarkOnion0 <Darkgenius1@protonmail.com>
2022-04-01 08:38:22 +0200

    switch the default desktop to plasma5 + bismuth

--- home/nixos/system/x11.nix
+++ home/nixos/system/x11.nix
@@ -18,36 +18,37 @@
   # For 32 bit applications
   hardware.opengl.driSupport32Bit = true;
 
-  # Enable the Plasma 5 Desktop Environment.
-  #services.xserver.displayManager.sddm.enable = true;
-  #services.xserver.desktopManager.plasma5.enable = true;
-
-  # Enable Gnome Desktop Environement
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
+  # Gnome specific settings
   #services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ];
   #services.dbus.packages = with pkgs; [ gnome2.GConf ];
+
+  # Cross DE / WM QT theming
   #qt5.enable = true;
   #qt5.platformTheme = "gtk2";
   #qt5.style = "gtk2";
-  programs.qt5ct.enable = true;
+  #programs.qt5ct.enable = true;
 
   # Configure keymap in X11
   services.xserver.layout = "fr";
   services.xserver.xkbOptions = "eurosign:e";
-
-  # i3
+	
+  # DE / WM settings
   services.xserver = {
     desktopManager = {
-      xterm.enable = false;
+      #xterm.enable = false; # I3
+      plasma5.enable = true; # Plasma
+      gnome.enable = false;
     };
    
     displayManager = {
-        defaultSession = "none+i3";
+    	sddm.enable = true; # Plasma
+        #defaultSession = "none+i3"; # I3
+	gdm.enable = false;
     };
 
+    # I3
     windowManager.i3 = {
-      enable = true;
+      enable = false;
       package = pkgs.i3-gaps;
     };
   };
