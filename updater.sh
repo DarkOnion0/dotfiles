dad1945bf72ce544812ea1a117b3b500294e159b
DarkOnion0 <Darkgenius1@protonmail.com>
2022-04-09 18:11:08 +0200

    update the sync script to track the new lua files

--- updater.sh
+++ updater.sh
@@ -2,16 +2,27 @@
 
 git status
 
+# VM
 cp ~/.config/i3/config ~/Programmation/git/dotfiles/home/.config/i3/
-cp ~/.config/fish/config.fish ~/Programmation/git/dotfiles/home/.config/fish/
 #cp ~/.config/rofi/* ~/Programmation/git/dotfiles/.config/rofi/
 #cp ~/.config/polybar/* ~/Programmation/git/dotfiles/.config/polybar/
+
+# SHELL
+cp ~/.config/fish/config.fish ~/Programmation/git/dotfiles/home/.config/fish/
 cp ~/.config/starship.toml ~/Programmation/git/dotfiles/home/.config/
 cp ~/.config/alacritty/alacritty.yml ~/Programmation/git/dotfiles/home/.config/alacritty/
 cp ~/.tmux.conf ~/Programmation/git/dotfiles/home/
+
+# GIT
 cp ~/.gitconfig ~/Programmation/git/dotfiles/home/
+
+# Neovim
 cp ~/.config/nvim/init.vim ~/Programmation/git/dotfiles/home/.config/nvim/init.vim
+cp ~/.config/nvim/init.lua ~/Programmation/git/dotfiles/home/.config/nvim/init.lua
+cp -r ~/.config/nvim/lua ~/Programmation/git/dotfiles/home/.config/nvim/
 cp ~/.config/nvim/coc-settings.json ~/Programmation/git/dotfiles/home/.config/nvim/coc-settings.json
+
+# OS
 sudo cp -r /etc/nixos/* ~/Programmation/git/dotfiles/home/nixos/
 
 git status
