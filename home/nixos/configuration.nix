1e4f622e2a55e8ba01203d3c90fcec81fdd3c592
DarkOnion0 <Darkgenius1@protonmail.com>
2022-04-25 21:41:28 +0200

    nixos: reformat file

--- home/nixos/configuration.nix
+++ home/nixos/configuration.nix
@@ -14,7 +14,7 @@
       ./network
       ./system
     ];
-
+  
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
