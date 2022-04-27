582402cd9924932e2e9324c7f369623c4a281a12
DarkOnion0 <Darkgenius1@protonmail.com>
2022-04-27 23:09:04 +0200

    i3: update scripts

--- home/.config/i3/bin/wallpaper.bash
+++ home/.config/i3/bin/wallpaper.bash
@@ -1,5 +1,5 @@
 file=`find ~/.wallpapers/ -type f -exec file {} \; | grep -o -P '^.+: \w+ image' | shuf -n 1 | sed "s|:.*||g"`
 
-convert $file -quality 100 /tmp/actual_wallpaper.png
+feh --recursive --bg-fill $file
 
-feh --recursive --bg-fill /tmp/actual_wallpaper.png
+convert $file -quality 10 -blur "5x4" /tmp/actual_wallpaper.png
