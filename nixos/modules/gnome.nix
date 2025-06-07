e8291e5afa3b1707ae687a4540d72533a0e8f4fe
DarkOnion0 <darkonion0@clapot.org>
2025-06-07 10:20:11 +0200

    fix(nixos/modules/gnome): update deprecated package name

--- nixos/modules/gnome.nix
+++ nixos/modules/gnome.nix
@@ -5,7 +5,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  services.udev.packages = [pkgs.gnome.gnome-settings-daemon];
+  services.udev.packages = [pkgs.gnome-settings-daemon];
 
   # Uniformize theming
   qt = {
